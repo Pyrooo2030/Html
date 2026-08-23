b14cf9a702fad0d55de5aa136f735e5cbebf8107
Pyrooo2030 <allysback2019@gmail.com>
2026-08-23 11:16:47 +0800

    added models and configure DB connection

---
+++ BackEnd/BackEnd/Models/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace BackEnd.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public string Sex { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
